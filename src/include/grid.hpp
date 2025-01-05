d21f6ee752250de06164f4b55dcb823660585ffd
superloubou <superloubou555@gmail.com>
2025-01-05 23:50:53 +0000

    add helper functions to grid

--- src/include/grid.hpp
+++ src/include/grid.hpp
@@ -1,34 +1,61 @@
 #pragma once
 
 #include <cell.hpp>
-#include <stdexcept>
 #include <SFML/Graphics.hpp>
-
 
 class Grid
 {
   private:
     size_t rows, cols;
 
-    size_t index(size_t row, size_t col) const
+    int index(size_t row, size_t col) const
     {
         if (row >= rows || col >= rows)
         {
-            throw std::out_of_range("Grid index out of bounds");
+            return -1;
         }
         return row * cols + col;
     }
 
     std::vector<Cell> data;
+    Cell invalidCell;
 
   public:
-    Grid(size_t rows, size_t cols) : rows(rows), cols(cols) {
-        data.resize(rows * cols);
+    Grid(size_t rows, size_t cols) : rows(rows), cols(cols), invalidCell(-1) { data.resize(rows * cols); }
+
+    Cell &getAt(size_t row, size_t col)
+    {
+        auto indexToGet = index(row, col);
+        if (indexToGet < 0)
+        {
+            return invalidCell;
+        }
+        else
+        {
+            return data.at(indexToGet);
+        }
     }
 
-    Cell &at(size_t row, size_t col) { return data.at(index(row, col)); }
+    std::vector<Cell> getNeighbors(size_t row, size_t col)
+    {
+        std::vector<Cell> neighbors;
+
+        const int directions[8][2] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+
+        for (const auto &dir : directions)
+        {
+            size_t neighborRow = row + dir[0];
+            size_t neighborCol = col + dir[1];
+
+            if (neighborRow < rows && neighborCol < cols && neighborCol >= 0 && neighborRow >= 0)
+            {
+                neighbors.push_back(getAt(neighborRow, neighborCol));
+            }
+        }
+
+        return neighbors;
+    }
 
     size_t numRows() const { return rows; }
     size_t numCols() const { return cols; }
-
 };
