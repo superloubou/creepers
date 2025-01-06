f96cdc5499c3eb1a6d0c1dcbb622e37918617881
superloubou <superloubou555@gmail.com>
2025-01-06 22:03:22 +0000

    calculate score

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@ int main()
     Renderer renderer;
 
     Grid grid(rows, cols);
+    grid.getAt(64, 64).cellType = 1;
 
     while (window.isOpen())
     {
@@ -25,11 +26,42 @@ int main()
         }
 
         window.clear();
-        
-        renderer.drawGrid(window,grid);
+
+        renderer.drawGrid(window, grid);
 
         window.display();
     }
 
     return 0;
+}
+
+int calculateScore(Grid &grid)
+{
+    int score = 0;
+    for (int r = 0; r < grid.numRows(); r++)
+    {
+        for (int c = 0; c < grid.numCols(); c++)
+        {
+            auto currentCell = grid.getAt(r, c);
+            auto neighbours = grid.getNeighbors(r, c);
+            int stemNeighbours = 0,petalNeighbours = 0;
+            for (auto cell: neighbours)
+            {
+                if (cell.cellType == 1) stemNeighbours += 1;
+                if (cell.cellType == 2) petalNeighbours += 1;
+            }
+
+            if (currentCell.cellType == 1)
+            {
+                score -= 1;
+            }
+
+            if (currentCell.cellType == 2)
+            {
+                score += 6;
+                score -= stemNeighbours * 2;
+            }
+        }
+    }
+    return score;
 }
